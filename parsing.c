57e99f26520fba99658b01ac540ed0d887b0ed06
Povilas Balzaravicius Pawka <povilas@uber.com>
2015-11-14 14:25:56 +0200

    Created lval structs, enums and functions

--- parsing.c
+++ parsing.c
@@ -20,6 +20,58 @@ void add_history(char* unused) {}
 #include <editline/history.h>
 #endif
 
+typedef struct {
+    int type;
+    long num;
+    int err;
+} lval;
+
+/* Possible lval types */
+enum { LVAL_NUM, LVAL_ERR };
+/* Possible error types */
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+/* Create new number type lval */
+lval lval_num(long x) {
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = x;
+    return v;
+}
+
+/* Create new error type lval */
+lval lval_err(int x) {
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = x;
+    return v;
+};
+
+void lval_print(lval v) {
+    switch (v.type) {
+        case LVAL_NUM:
+            printf("%li", v.num);
+            break;
+
+        case LVAL_ERR:
+            if (v.type == LERR_DIV_ZERO) {
+                printf("Error: Division by zero!");
+            } 
+            if (v.type == LERR_BAD_NUM) {
+                printf("Error: Invalid number!");
+            }
+            if (v.type == LERR_BAD_OP) {
+                printf("Error: Invalid operator!");
+            }
+            break;
+    }
+}
+
+void lval_println(lval v) {
+    lval_print(v);
+    printf("\n");
+}
+
 long eval_op(long x, char* op, long y) {
     if (strcmp(op, "+") == 0) { return x + y; }
     if (strcmp(op, "-") == 0) { return x - y; }
